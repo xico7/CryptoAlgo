7c7ae989d661e58cbe3ee2cc3ebdbfd120d1105a
fcs <francisco.conceicaosilva7@gmail.com>
2022-02-16 10:18:03 +0000

    ...

--- main.py
+++ main.py
@@ -57,9 +57,13 @@ async def ta_analysis():
             if current_minute != cur_time:
                 current_minute = cur_time
 
-                if current_minute % 1800 == 0:
+                if current_minute % 1 == 0:
+                # TODO: if current_minute % 1800 == 0:
                     # if finished_ohlc_open_timestamp % mongoDBcreate.THIRTY_MIN_IN_SEC == 0 and \
                     #         finished_ohlc_open_timestamp > (begin_run + mongoDBcreate.ONE_DAY_IN_SEC):
+                    coin_trades_feed = mongo.connect_to_ta_lines_db()
+                    for elem in coin_trades_feed.list_collection_names():
+                        pass
                     ta_cache.ta_chart = dts.create_last_day_rs_chart(current_minute, get_db_coins_momentprice)
 
             pass
